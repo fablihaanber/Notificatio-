0dfcb9850eb6d3e36118ce773e7bfc1513263d3d
fablihaanber <fablihaanber41@gmail.com>
2019-11-19 13:13:29 -0700

    Added permission

--- app/src/main/java/com/example/notaficatio/MainActivity.java
+++ app/src/main/java/com/example/notaficatio/MainActivity.java
@@ -1,18 +1,27 @@
 package com.example.notaficatio;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.app.NotificationManagerCompat;
+import androidx.core.content.ContextCompat;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -32,6 +41,8 @@ public class MainActivity extends AppCompatActivity {
     int notifyId1 = 1234;
     int notifyId2 = 5678;
     int notifyId3 = 9678;
+    private int LOCATION_PERMISSION_CODE=133;
+    Button btnRq;
     NotificationManagerCompat notificationManagerCompat;
     String ar3="shahbag";
     @RequiresApi(api = Build.VERSION_CODES.O)
@@ -39,6 +50,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnRq=findViewById(R.id.button);
+        btnRq.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION)== PackageManager.PERMISSION_GRANTED){
+                    Toast.makeText(MainActivity.this,"Permission granted alredy",Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    requestLocationPermission();
+                }
+
+
+            }
+        });
         list=new ArrayList<MyDoes>();
         sendOnChannel1("shakalaka","boom");
         reference= FirebaseDatabase.getInstance().getReference().child("DoesApp");
@@ -75,6 +100,45 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+    private void requestLocationPermission(){
+
+        if(ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.ACCESS_FINE_LOCATION)){
+            new AlertDialog.Builder(this)
+                    .setTitle("Location permission")
+                    .setMessage("Allow Locatio app to access this devices location?")
+                    .setPositiveButton("ALLOW", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            ActivityCompat.requestPermissions(MainActivity.this,
+                                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                                    LOCATION_PERMISSION_CODE);
+
+                        }
+                    })
+                    .setNegativeButton("DENY", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    }).create().show();
+
+
+        }else{
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},LOCATION_PERMISSION_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode==LOCATION_PERMISSION_CODE){
+            if(grantResults.length>0&&grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(MainActivity.this,"Permission Granted",Toast.LENGTH_SHORT).show();
+            }
+            else{
+                Toast.makeText(MainActivity.this,"Permission Denied",Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
     @RequiresApi(api = Build.VERSION_CODES.O)
